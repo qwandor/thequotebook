1b04dc039f7bb06f1e61c0f57b3f7b323e886dcd
Andrew Walbran <qwandor@gmail.com>
2008-12-28 00:01:53 +1300

    Database migrated

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081227045445) do
+ActiveRecord::Schema.define(:version => 20081227105616) do
 
   create_table "contexts", :force => true do |t|
     t.string   "name"
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "fullname"
+    t.string   "openid"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
