545e620c73b627d3a01d139d2f5e4ee9d303a3b0
Andrew Walbran <qwandor@gmail.com>
2008-12-27 17:56:12 +1300

    Scaffolding for contexts

---
+++ db/migrate/20081227045445_create_contexts.rb
@@ -0,0 +1,14 @@
+class CreateContexts < ActiveRecord::Migration
+  def self.up
+    create_table :contexts do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contexts
+  end
+end
