e47b1ce0398322145faf51bf64d4e2745f61f7a2
Andrew Walbran <qwandor@gmail.com>
2008-12-28 15:20:10 +1300

    Username must be unique irrespective of case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates_presence_of :username
   validates_length_of :username, :minimum => 3
-  validates_uniqueness_of :username
+  validates_uniqueness_of :username, :case_sensitive => false
 end
