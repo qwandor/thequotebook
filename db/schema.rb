976a9b0960f39442be1b375a418e18042e9e7544
Andrew Walbran <qwandor@gmail.com>
2009-01-10 15:23:57 +1300

    Database tables generated for OpenID

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081230052010) do
+ActiveRecord::Schema.define(:version => 20090110011107) do
 
   create_table "contexts", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "open_id_authentication_associations", :force => true do |t|
+    t.integer "issued"
+    t.integer "lifetime"
+    t.string  "handle"
+    t.string  "assoc_type"
+    t.binary  "server_url"
+    t.binary  "secret"
+  end
+
+  create_table "open_id_authentication_nonces", :force => true do |t|
+    t.integer "timestamp",  :null => false
+    t.string  "server_url"
+    t.string  "salt",       :null => false
   end
 
   create_table "quotes", :force => true do |t|
