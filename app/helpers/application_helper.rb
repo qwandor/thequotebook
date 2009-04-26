c0481150e4d2d399a29f46892d96effe319c78fe
Andrew Walbran <qwandor@gmail.com>
2009-04-26 17:38:21 +1200

    Nice default time format too

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,3 +4,4 @@ end
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:long] = '%A %d %B %Y at %I:%M %P %Z'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:short] = '%Y-%m-%d, %I:%M %P'
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:default] = '%A %d %B %Y at %I:%M %P'
