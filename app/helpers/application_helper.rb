265d3ebc38cce1571029b42fb7de130299123122
Andrew Walbran <qwandor@gmail.com>
2009-04-26 17:27:16 +1200

    Better time formats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 end
+
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:long] = '%A %d %B %Y at %I:%M %P %Z'
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:short] = '%Y-%m-%d, %I:%M %P'
