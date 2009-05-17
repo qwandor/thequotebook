3531e88f7baae2dc97283de9f8fad503ea4b71d6
Andrew Walbran <qwandor@gmail.com>
2009-05-17 21:35:26 +1200

    Showing a couple more top quotebooks on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @latest_quotes = Quote.all(:order => 'created_at desc', :limit => 5)
-    @top_contexts = Context.all(:select => 'id, name, description, (select count(*) from quotes where context_id = contexts.id) as quote_count', :order => 'quote_count desc', :limit => 3)
+    @top_contexts = Context.all(:select => 'id, name, description, (select count(*) from quotes where context_id = contexts.id) as quote_count', :order => 'quote_count desc', :limit => 5)
     if logged_in?
       @current_user_contexts = current_user.contexts
       @current_user_quotes = Quote.all(:conditions => ['context_id in (?)', current_user.context_ids], :order => 'created_at desc', :limit => 5)
