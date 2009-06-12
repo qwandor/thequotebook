686ce9ba51e84f85b14b0c01f8fdfe574caca620
Andrew Walbran <qwandor@gmail.com>
2009-06-12 23:22:49 +1200

    Sending email notifications about comments too

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -9,4 +9,13 @@ class UserMailer < ActionMailer::Base
     sent_on Time.now
     body({:quote => quote})
   end
+
+  #Send an email notification to the quoter and quotee that a new comment has been added to the quote
+  def comment_email(comment, recipient)
+    recipients recipient.email_address
+    from 'theQuotebook <notifications@thequotebook.net>'
+    subject "#{comment.user.fullname} commented on your quote in #{comment.quote.context.name}"
+    sent_on Time.now
+    body({:comment => comment, :recipient => recipient, :quote => comment.quote})
+  end
 end
