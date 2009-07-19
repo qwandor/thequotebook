a0163fc543d8eb2f585185753608a2b9b7f334ea
Andrew Walbran <qwandor@gmail.com>
2009-07-19 17:36:31 +1200

    protect_from_forgery needs a secret, now that we are not using the cookie session store.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -8,11 +8,11 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '0af2f8aed03e9e73d9772a54d5cd75fa'
-  
-  # See ActionController::Base for details 
+  protect_from_forgery :secret => '0af2f8aed03e9e73d9772a54d5cd75fa' #Change this on the server
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
 
   before_filter :set_time_zone, :get_random_quote
