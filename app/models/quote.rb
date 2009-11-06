dc7c167f29c463abdb465515d8d98a1b07478fa0
Andrew Walbran <qwandor@gmail.com>
2009-11-06 20:44:36 +1300

    Fixing bug deleting quotes with comments.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,7 @@
 class Quote < ActiveRecord::Base
   validates_presence_of :quoter, :quotee, :context
   validates_length_of :quote_text, :minimum => 3
-  has_many :comments, :order => 'created_at ASC'
+  has_many :comments, :order => 'created_at ASC', :dependent => :delete_all
 
   after_create :send_notification
 
