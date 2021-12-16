8e620cd5358b0853777c5d28514a4697b04ef982
Andrew Walbran <qwandor@gmail.com>
2021-12-16 18:05:20 +0000

    Set up axum.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use eyre::Report;
+
+#[tokio::main]
+async fn main() -> Result<(), Report> {
+    stable_eyre::install()?;
+    pretty_env_logger::init();
+    color_backtrace::install();
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse()?)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
