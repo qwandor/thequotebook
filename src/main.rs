c3f9824927c32e07c9525ee0903daaedeb84ca29
Andrew Walbran <qwandor@gmail.com>
2021-12-16 20:24:38 +0000

    Serve static images and stylesheets.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod index;
 
-use axum::{routing::get, Router};
+use axum::{
+    http::StatusCode,
+    routing::{get, get_service},
+    Router,
+};
 use eyre::Report;
+use std::io;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
@@ -9,7 +15,30 @@ async fn main() -> Result<(), Report> {
     pretty_env_logger::init();
     color_backtrace::install();
 
-    let app = Router::new().route("/", get(index::handle));
+    let app = Router::new()
+        .route("/", get(index::handle))
+        .nest(
+            "/images",
+            get_service(ServeDir::new("public/images")).handle_error(
+                |error: io::Error| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal error: {}", error),
+                    )
+                },
+            ),
+        )
+        .nest(
+            "/stylesheets",
+            get_service(ServeDir::new("public/stylesheets")).handle_error(
+                |error: io::Error| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal error: {}", error),
+                    )
+                },
+            ),
+        );
 
     axum::Server::bind(&"0.0.0.0:3000".parse()?)
         .serve(app.into_make_service())
