085c53c43f955d6bb5326b605f71e2583d4f8c49
Andrew Walbran <qwandor@gmail.com>
2021-12-19 13:28:58 +0000

    Show real contexts on /contexts.

--- src/controllers/contexts.rs
+++ src/controllers/contexts.rs
@@ -3,16 +3,28 @@ use crate::{
     types::{Context, Flash},
 };
 use askama::Template;
-use axum::response::Html;
+use axum::{extract::Extension, response::Html};
+use sqlx::{Pool, Postgres};
 
-pub async fn index() -> Result<Html<String>, InternalError> {
+pub async fn index(
+    Extension(pool): Extension<Pool<Postgres>>,
+) -> Result<Html<String>, InternalError> {
+    let contexts = sqlx::query_as::<_, Context>(
+        "SELECT contexts.*,
+           (SELECT COUNT(*) FROM quotes WHERE quotes.context_id = contexts.id) as quotes_count
+         FROM contexts
+         ORDER BY contexts.created_at DESC",
+    )
+    .fetch_all(&pool)
+    .await?;
+
     let template = IndexTemplate {
         flash: Flash {
             notice: None,
             error: None,
         },
         logged_in: false,
-        contexts: vec![],
+        contexts,
     };
     Ok(Html(template.render()?))
 }
