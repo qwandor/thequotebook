29898e5e41f56aae730ae36d7a7d584b5d7da196
Andrew Walbran <qwandor@gmail.com>
2021-12-23 00:32:21 +0000

    Helper methods on Session.

--- src/session.rs
+++ src/session.rs
@@ -1,3 +1,4 @@
+use crate::model::User;
 use axum::{
     async_trait,
     body::Body,
@@ -11,6 +12,16 @@ pub struct Session {
     pub logged_in: bool,
     pub current_user_id: i32,
     pub current_user_fullname: String,
+}
+
+impl Session {
+    pub fn logged_in(&self) -> bool {
+        self.logged_in
+    }
+
+    pub fn is_current_user(&self, &user_id: &i32) -> bool {
+        self.current_user_id == user_id
+    }
 }
 
 #[derive(Default, Debug, Eq, PartialEq)]
