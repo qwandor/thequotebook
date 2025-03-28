02aeea322a704424f1ccba8d2914ecf5fea6e7cf
Andrew Walbran <qwandor@gmail.com>
2025-03-28 11:22:46 +0000

    Fixes after askama update.

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -3,7 +3,7 @@ mod quote;
 mod time;
 mod user;
 
-use askama::Html;
+use askama::filters::Html;
 pub use comment::comment_format;
 pub use quote::{
     chatty_quote, comment_title_quote, formatted_quote, formatted_single_quote,
@@ -13,7 +13,7 @@ pub use time::long_datetime;
 pub use user::{gravatar_for, link_to_user};
 
 fn escape(text: &str) -> String {
-    askama::filters::escape(Html, text).unwrap().to_string()
+    askama::filters::escape(text, Html).unwrap().to_string()
 }
 
 #[cfg(test)]
@@ -22,7 +22,7 @@ mod tests {
 
     #[test]
     fn test_escape() {
-        assert_eq!(escape("<a>&b"), "&lt;a&gt;&amp;b");
-        assert_eq!(escape("\"'"), "&quot;&#x27;");
+        assert_eq!(escape("<a>&b"), "&#60;a&#62;&#38;b");
+        assert_eq!(escape("\"'"), "&#34;&#39;");
     }
 }
